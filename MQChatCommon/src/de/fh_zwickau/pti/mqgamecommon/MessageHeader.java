f83be460b568597fa1841cbc9f1698429bc9f91a
mak11188 <mak11188@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-05-15 05:51:13 +0000

    initial commit for chatCommon

---
+++ MQChatCommon/src/de/fh_zwickau/pti/mqgamecommon/MessageHeader.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.fh_zwickau.pti.mqgamecommon;
+
+/**
+ * @author georg beier
+ *
+ */
+public enum MessageHeader {
+	MsgKind,
+	LoginUser,
+	LoginPassword,
+	AuthToken,
+	RefID,
+	ChatroomID,
+	ChatterNickname
+}
---
+++ MQChatCommon/src/de/fh_zwickau/pti/mqgamecommon/StompReference.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.fh_zwickau.pti.mqgamecommon;
+
+/**
+ * data structure to identify an object by its id and the stomp destination where it can be
+ * reached
+ * 
+ * @author georg beier
+ * 
+ */
+public class StompReference {
+	public String id;
+	public String destination;
+}
