d0a6a570e5b981bb24cf4594236b76d200135ed3
map111ks <map111ks@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-05-21 11:50:12 +0000

    MergeChatSwingclient jmsadapter update 2 testklasse

---
+++ trunk/ChatClient/src/Test/TestAismael.java
@@ -0,0 +1,36 @@
+package Test;
+
+import gui.ChatSwingClient;
+
+import javax.jms.JMSException;
+
+import de.fh_zwickau.pti.jms.userservice.AuthenticationServer;
+import de.fh_zwickau.pti.jms.userservice.chat.ChatServer;
+import messaging.ChatJmsAdapter;
+
+public class TestAismael {
+
+	public static void main(String[] args) throws Exception {
+		
+		//AuthenticationServer.main(args);
+		//ChatServer.main(args);
+		
+		ChatJmsAdapter CJA=ChatJmsAdapter.getInstance();
+		CJA.setMessageReceiver(ChatSwingClient.getInstance());
+
+		String brokerUri;
+		if (args.length == 0) {
+			String localConnection = "tcp://localhost:61616";
+			brokerUri = localConnection;
+		} else {
+			brokerUri = args[0];
+		}
+		CJA.connectToServer(brokerUri);
+		CJA.register("xy", "xy");
+		CJA.register("xy", "xy");
+		CJA.logout();
+		
+
+	}
+
+}
