b928b0c68990c7b6e335a3218162393bce5cac04
anf111bw <anf111bw@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-05-21 09:47:56 +0000

    window implements runnable -- start is now in test class

--- ChatClient/src/Test/Test.java
+++ ChatClient/src/Test/Test.java
@@ -1,4 +1,5 @@
 package Test;
+import gui.SwingWindow;
 import States.StatesClasses.NotLoggedIn;
 
 
@@ -7,6 +8,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Create GUI and run it on a new Thread
+		SwingWindow window = new SwingWindow();
+		Thread threadWindow = new Thread(window);
+		threadWindow.start();
 	}
 
 }
