db7244b7d2d141380cbe8831871b3248000feffb
anf111bw <anf111bw@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-05-21 11:19:06 +0000

    Added actions to chatclient

--- ChatClient/src/Test/Test.java
+++ ChatClient/src/Test/Test.java
@@ -1,4 +1,5 @@
 package Test;
+import gui.ChatSwingClient;
 import gui.SwingWindow;
 import States.StatesClasses.NotLoggedIn;
 
@@ -8,10 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Create GUI and run it on a new Thread
-		SwingWindow window = new SwingWindow();
-		Thread threadWindow = new Thread(window);
-		threadWindow.start();
+		// Start client
+		ChatSwingClient.getInstance();
 	}
 
 }
