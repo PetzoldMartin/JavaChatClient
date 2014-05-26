92be8285c750f46bee87436502dfcfa750602223
pee111q8 <pee111q8@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-05-26 19:37:45 +0000

    edit methodbodys deny() and onAcceptInvitation() in class Invited

--- ChatClient/src/States/StatesClasses/WaitingStates/Invited.java
+++ ChatClient/src/States/StatesClasses/WaitingStates/Invited.java
@@ -1,15 +1,40 @@
 package States.StatesClasses.WaitingStates;
 
+import javax.jms.JMSException;
+
 import States.ChatClientState;
+import States.StatesClasses.LoggedIn;
 
-public abstract class Invited extends AbstractWaiting {
+public class Invited extends AbstractWaiting {
 
+	/**
+	 * @param oldState
+	 */
 	public Invited(ChatClientState oldState) {
 		super(oldState);
 		// TODO Auto-generated constructor stub
 	}
 
-	public abstract void onDeny();
+	/**
+	 * deny an invite
+	 */
+	public void onDeny(){
+		try {
+			messageProducer.deny();
+			new LoggedIn(this);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-	public abstract void onAcceptInvitataion();
+	public void onAcceptInvitataion() {
+		try {
+			messageProducer.acceptInvitation();
+			new LoggedIn(this);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
