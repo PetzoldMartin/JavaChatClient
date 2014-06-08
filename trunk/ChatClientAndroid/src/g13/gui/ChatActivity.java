ce0f776f77584c81b876fbc849387df5206d0c91
anf111bw <anf111bw@f4f58aa8-e71b-4499-9a64-9a61ae68935f>
2014-06-08 14:46:24 +0000

    New Debug mode and new chat activity

---
+++ trunk/ChatClientAndroid/src/g13/gui/ChatActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package g13.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author foxel
+ *
+ */
+public class ChatActivity extends MainActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_own_chat);
+	}
+}
--- trunk/ChatClientAndroid/src/g13/gui/LoggedInActivity.java
+++ trunk/ChatClientAndroid/src/g13/gui/LoggedInActivity.java
@@ -3,12 +3,31 @@
  */
 package g13.gui;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 /**
  * @author foxel
  *
  */
 public class LoggedInActivity extends MainActivity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loggedin);
+		
+		findViewById(R.id.create).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// FIXME: DEBUG IF ELSE
+				if(isTestGUI) SetActivity(ChatActivity.class);
+				//else // TODO:
+			}
+			
+		});
+	}
+		
 }
